0b8ddea7fffc19d674f1a9f529ea48f5a98abc98
naumchuk0 <mn645680@gmail.com>
2024-04-14 14:26:30 +0100

    add create category form

--- app/src/main/java/com/example/firstandroidproject/network/CategoriesApi.java
+++ app/src/main/java/com/example/firstandroidproject/network/CategoriesApi.java
@@ -1,12 +1,18 @@
 package com.example.firstandroidproject.network;
+import com.example.firstandroidproject.dto.category.CategoryCreateDTO;
 import com.example.firstandroidproject.dto.category.CategoryItemDTO;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface CategoriesApi {
     @GET("/api/categories")
     public Call<List<CategoryItemDTO>> list();
+
+    @POST("/api/categories")
+    public Call<CategoryItemDTO> create(@Body CategoryCreateDTO model);
 }
